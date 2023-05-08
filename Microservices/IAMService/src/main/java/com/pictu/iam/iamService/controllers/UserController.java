7fda90ab5d96a8126e1639e11c324278b65ef9b0
Anurag Kumar <anuragkr41@gmail.com>
2023-05-09 00:49:13 +0530

    Reslience 4J and circuit breakers implemented

--- Microservices/IAMService/src/main/java/com/pictu/iam/iamService/controllers/UserController.java
+++ Microservices/IAMService/src/main/java/com/pictu/iam/iamService/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.pictu.iam.iamService.controllers;
 import com.pictu.iam.iamService.entities.User;
 import com.pictu.iam.iamService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
 
     @Autowired
     public UserService userService;
@@ -19,10 +21,21 @@ public class UserController {
     }
     // Get a single User
     @GetMapping("/{userId}")
+    @CircuitBreaker(name="photoBreaker", fallbackMethod = "photoFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user = userService.getSingleUser(userId);
         return ResponseEntity.ok(user);
     }
+
+    //Creating fallback method for circuit breaker
+    public ResponseEntity<User> photoFallback(String userId, Exception ex){
+        System.out.println("Fallback is executed==============Service Down "+ex.getMessage());
+        User user = User.builder().email("dummy@gmail.com").name("Dummy").password("asdfasdf").build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+
     //Get All Users
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
--- Microservices/IAMService/src/main/java/com/pictu/iam/iamService/entities/User.java
+++ Microservices/IAMService/src/main/java/com/pictu/iam/iamService/entities/User.java
@@ -1,9 +1,6 @@
 package com.pictu.iam.iamService.entities;
 import com.pictu.core.payloads.AuditableEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -14,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 public class User extends AuditableEntity {
     @Id
     private String userId;
